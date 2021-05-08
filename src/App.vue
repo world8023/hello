bddb849e3eb02c262ae93a2b881abf27c3e7b5d3
world8023 <395059495@qq.com>
2021-05-08 16:05:20 +0800

    Update App.vue
    欢迎来到苏晓的网站

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="欢迎来到苏晓的网站"/>
   </div>
 </template>
 
